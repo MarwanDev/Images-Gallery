9f9311a8c5fabb40773bd4f4757e3fce04271fda
Marwan Abdelsattar <marwaneg@aucegypt.edu>
2022-08-09 23:24:01 +0200

    initial header component

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header'
+function App() {
+  return (
+    <div className="App">
+      <Header title="Images Gallery"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Navbar} from 'react-bootstrap'
+const Header = ({title}) => {
+    return(
+    <Navbar bg="light" variant="light">
+      <Navbar.Brand href="/">{title}</Navbar.Brand>
+  </Navbar> 
+    )
+};
+
+export default Header 
