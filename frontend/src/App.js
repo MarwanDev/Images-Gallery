072a2521821fcc5d073b681409e714e7fcd48f28
Marwan Abdelsattar <marwaneg@aucegypt.edu>
2022-08-10 15:43:50 +0200

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const UNSPLASH_KEY = 't11X0zZNEoDJNqc7e3M0Uj3RIV37FQAzf5g9yhk9k8U';
 
 const App=()=> {
   const [word, setWord] = useState('');
@@ -10,6 +11,14 @@ const App=()=> {
   const handleSearchSubmit = (e) =>{
     e.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    .then((res)=>res.json())
+    .then((data)=>{
+      console.log(data);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
   }
 
   return (
