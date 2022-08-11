c701d7c8a23dd8e920cb2a01fff7028f4cd386af
Marwan Abdelsattar <marwaneg@aucegypt.edu>
2022-08-11 20:24:34 +0200

    added request to the unsplash api

---
+++ api/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+import requests
+
+UNSPLASH_URL='https://api.unsplash.com/photos/random'
+UNSPLASH_KEY='t11X0zZNEoDJNqc7e3M0Uj3RIV37FQAzf5g9yhk9k8U'
+
+if not UNSPLASH_KEY:
+  raise EnvironmentError("Please insert an UNSPLASH_KEY")
+app = Flask(__name__)
+
+@app.route("/new-image")
+def new_image():
+    word=request.args.get("query")
+
+    headers = {
+    "Accept-Version": "v1",
+    "Authorization": "Client-ID " + UNSPLASH_KEY
+    }
+    params = {
+      "query": word
+    }
+    response=requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    data=response.json()
+    return data
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5050)
