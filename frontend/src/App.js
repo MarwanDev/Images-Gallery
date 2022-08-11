42f9c2586f295bfc2fe3ee2b9d343ed3b90d703c
Marwan Abdelsattar <marwaneg@aucegypt.edu>
2022-08-11 15:09:33 +0200

    welcome component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Search from './components/Search';
 import ImageCard from './components/ImageCard';
 import { Container, Row, Col } from 'react-bootstrap';
+import Welcome from './components/Welcome';
 const UNSPLASH_KEY = 't11X0zZNEoDJNqc7e3M0Uj3RIV37FQAzf5g9yhk9k8U';
 
 const App = () => {
@@ -35,13 +36,17 @@ const App = () => {
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
       <Container className="mt-4">
-        <Row xs={1} md={2} lg={3}>
-          {images.map((image, i) => (
-            <Col key={i} className="pb-3">
-              <ImageCard image={image} deleteImage={handleDeleteImage} />
-            </Col>
-          ))}
-        </Row>
+        {images.length ? (
+          <Row xs={1} md={2} lg={3}>
+            {images.map((image, i) => (
+              <Col key={i} className="pb-3">
+                <ImageCard image={image} deleteImage={handleDeleteImage} />
+              </Col>
+            ))}
+          </Row>
+        ) : (
+          <Welcome />
+        )}
       </Container>
     </div>
   );
---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const Welcome = () => (
+  <div class="container-fluid bg-light text-dark p-5">
+    <h1>Images Gallery</h1>
+    <p>
+      This is an app that retrieves photos using Unsplash API. Start searching
+      by entering words in the input field.
+    </p>
+    <p>
+      <Button
+        variant="primary"
+        href="https://unsplash.com"
+        target="_blank"
+      >Learn More</Button>
+    </p>
+  </div>
+);
+export default Welcome;
