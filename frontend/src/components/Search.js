cc16279e61450dd14c77e3d2ff2fdbd911f0ee90
Marwan Abdelsattar <marwaneg@aucegypt.edu>
2022-08-26 15:39:43 +0200

    docker compose

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -19,7 +19,7 @@ const Search = ({ word, setWord, handleSubmit }) => {
           </Col>
           <Col>
             <Button variant="primary" type="submit">
-              Search
+              Bala7
             </Button>
           </Col>
         </Row>
